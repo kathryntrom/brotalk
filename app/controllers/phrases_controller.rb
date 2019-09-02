c912bb57ac3555bd606e0559b508ac8acc969107
Kathryn Tromba <kathryntromba@gmail.com>
2019-09-02 14:22:42 +0000

    Initial commit

---
+++ app/controllers/phrases_controller.rb
@@ -0,0 +1,22 @@
+class PhrasesController < ApplicationController
+
+  def index
+  	@phrases = Phrase.all
+  end
+
+  def new
+  	@phrase = Phrase.new
+  end
+
+  def create
+  	Phrase.create(phrase_params)
+  	redirect_to root_path
+  end
+
+  private
+
+  def phrase_params
+  	params.require(:phrase).permit(:name)
+  end
+  
+end
---
+++ db/migrate/20190902135648_create_phrases.rb
@@ -0,0 +1,8 @@
+class CreatePhrases < ActiveRecord::Migration[5.2]
+  def change
+    create_table :phrases do |t|
+      t.string :word
+      t.timestamps
+    end
+  end
+end
